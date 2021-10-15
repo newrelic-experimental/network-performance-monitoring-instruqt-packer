de5cd6cfcfb400d9dab83d475aa5aa740020b4c0
Alex Ronquillo <aronquillo@newrelic.com>
2021-10-15 12:28:26 -0400

    Remove duplicate containers

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -7,19 +7,8 @@ docker network create -d overlay --subnet=10.10.0.0/24 --attachable testnet
 
 git clone https://github.com/newrelic-experimental/network-performance-monitoring-instruqt-packer
 
-docker run -d --name=cisco-router-1 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-router-1:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-router-2 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-router-2:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-router-3 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-router-3:/usr/local/snmpsim/data tandrup/snmpsim
+docker run -d --name=cisco-router --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-router:/usr/local/snmpsim/data tandrup/snmpsim
 
-docker run -d --name=cisco-switch-1 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-1:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-2 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-2:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-3 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-3:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-4 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-4:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-5 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-5:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-6 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-6:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-7 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-7:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-8 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-8:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-9 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-9:/usr/local/snmpsim/data tandrup/snmpsim
-docker run -d --name=cisco-switch-10 --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch-10:/usr/local/snmpsim/data tandrup/snmpsim
+docker run -d --name=cisco-switch --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/cisco-switch:/usr/local/snmpsim/data tandrup/snmpsim
 
 docker run -d --name=linksys-router --restart=always --network=testnet --privileged -v /:/rootfs:ro -v /etc/localtime:/etc/localtime:ro -v /home/packer/network-performance-monitoring-instruqt-packer/files/linksys-router:/usr/local/snmpsim/data tandrup/snmpsim
